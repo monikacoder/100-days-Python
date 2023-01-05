dbb1225e1dac3353886cf8bd43ef4647b966c07e
monikacoder <120687956+monikacoder@users.noreply.github.com>
2023-01-05 22:19:41 +0000

    Create Day 9_100.py

---
+++ Day 9_100.py
@@ -0,0 +1,19 @@
+year = int(input("Which year you were born? : "))
+
+if year >1925 and year <= 1946:
+    print("You belong to Traditionalists generation")                 
+elif year > 1947 and year <= 1964:
+   print("You belong to Baby Booers genration") 
+elif year > 1965 and year <= 1981:
+    print("You are a generation X")
+elif year > 1982 and year <= 1995:
+    print("You are just millenials") 
+elif year >1996 and year <= 2015:
+    print("The nascent generation Z")
+else:
+  print("you don't belong to any generation. Enjoy it!")
+     
+           
+            
+           
+                 
