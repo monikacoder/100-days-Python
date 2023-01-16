7cd7329331a5ce4269a79fa5d2fa82beb4016bfc
monikacoder <120687956+monikacoder@users.noreply.github.com>
2023-01-16 21:45:52 +0000

    Create Day 20_100.py

---
+++ Day 20_100.py
@@ -0,0 +1,7 @@
+
+start = int(input("Enter the starting number"))
+end = int(input("Enter the ending number"))
+increment = int(input("Enter the increment"))
+
+for i in range(start,end,increment):
+    print(i)
