608883dd29322ce6b85779857584d620bd4944e1
monikacoder <120687956+monikacoder@users.noreply.github.com>
2023-01-29 19:33:21 +0000

    Create Day 29_100.py

---
+++ Day 29_100.py
@@ -0,0 +1,8 @@
+print("\033[33m", end="") #yellow
+print("Super subroutine")
+print()
+print("With my","\033[35m", "new program", "\033[33m", "I can just call red('and')", "\033[31m", "and", "\033[33m", "that word will appear" , sep=" ")
+print()
+print("With no", "\033[32m", "weird gaps", sep=" ")
+print()
+print("\033[33m", "Epic.")
